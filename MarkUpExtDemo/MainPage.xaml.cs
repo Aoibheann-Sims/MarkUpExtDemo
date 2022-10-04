a2e83d9429b07040558c2c5a6e28d54d4201e17c
Aoibheann-Sims <78419991+Aoibheann-Sims@users.noreply.github.com>
2022-10-04 11:07:08 +0100

    Add project files.

---
+++ MarkUpExtDemo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MarkUpExtDemo;
+
+public partial class MainPage : ContentPage
+{
+	public const double MyFontSize = 25;
+	public MainPage()
+	{
+		InitializeComponent();
+		//btnChangeText.Clicked += OnbtnChangeTextClicked;
+	}
+}
+
+public class GlobalFontSizeExtension : IMarkupExtension
+{
+	public object ProvideValue(IServiceProvider serviceProvider)
+	{
+		return MainPage.MyFontSize;
+	}
+}
+
